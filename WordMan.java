6cb935cddc3ca259be8e9f646474eedeae58a45c
mrseiler <mrseiler@bsu.edu>
2018-04-01 22:58:44 -0400

    Third Commit

--- WordMan.java
+++ WordMan.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class WordMan {
 
-    Scanner scan = new Scanner(System.in);
+    Scanner words = new Scanner(System.in);
     final int guessesLeft = 10;
 
     static public void guess(String word, int guessesLeft) {
@@ -20,6 +20,33 @@ public class WordMan {
         }
         System.out.print(spaces);
         System.out.println("\t Number of guesses left: " + guessesLeft);
+
+        Scanner scan = new Scanner(System.in);
+
+        while(guessesLeft>0){
+            char input= scan.next().charAt(0);
+
+            if(word.contains(input + "")) {
+                for(int j=0; j < word.length(); j++) {
+                    if(word.charAt(j) == input) {
+                        spaces[j] = input;
+                    }
+                }
+            }
+            else {
+                guessesLeft--;
+            }
+            if(word.equals(String.valueOf(spaces))){
+                System.out.println(spaces);
+                System.out.println("WINNER WINNER CHICKEN DINNER");
+            }
+
+            System.out.print(spaces);
+            System.out.println("\t Number of guesses left: " + guessesLeft);
+        }
+        if(guessesLeft==0){
+            System.out.println("GAME OVER");
+        }
     }
 
 }
